7db5f4da87a88e9c7f056697e8e453d988b6a4d8
ambiennt <ambienntdev@gmail.com>
2022-07-01 21:40:00 -0700

    Update EmotePacket.h

--- MinecraftHeaders/Packet/EmotePacket.h
+++ MinecraftHeaders/Packet/EmotePacket.h
@@ -7,17 +7,17 @@
 class EmotePacket : public Packet {
 public:
 
-    enum class Flags : uint8_t {
-        NONE        = 0x0,
-        SERVER_SIDE = 0x1,
-    };
+	enum class Flags : uint8_t {
+		NONE        = 0x0,
+		SERVER_SIDE = 0x1,
+	};
 
 	ActorRuntimeID mRuntimeId; // 0x28
 	std::string mPieceId; // 0x30 - https://github.com/JustTalDevelops/bedrock-emotes
-    Flags mFlags; // 0x50 - actually a uint8_t in bds
+	Flags mFlags; // 0x50 - actually a uint8_t in bds
 
 	EmotePacket(ActorRuntimeID rid, std::string const& pieceId, EmotePacket::Flags flags = EmotePacket::Flags::NONE)
-        : mRuntimeId(rid), mPieceId(pieceId), mFlags(flags) {}
+		: mRuntimeId(rid), mPieceId(pieceId), mFlags(flags) {}
 	MCAPI virtual MinecraftPacketIds getId() const;
 	MCAPI virtual std::string getName() const;
 	MCAPI virtual void write(BinaryStream &) const;
